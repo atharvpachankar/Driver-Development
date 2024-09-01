670c00ca5201310c1a98f67f01e1e5e04fa0ea3f
atharvpachankar <104566276+atharvpachankar@users.noreply.github.com>
2024-09-01 17:11:27 +0530

    Create stm32f446rstmf446re_USART_driver.h

---
+++ stm32f446rstmf446re_USART_driver.h
@@ -0,0 +1,324 @@
+#ifndef INC_USART_DRIVER_H_
+#define INC_USART_DRIVER_H_
+
+#include "stm32f44643xx.h"
+
+/*
+ * Configuration structure for USARTx peripheral
+ */
+typedef struct
+{
+	uint8_t USART_WordLength;
+	uint8_t USART_Mode;
+	uint32_t USART_Baud;
+	uint8_t USART_NoOfStopBits;
+	uint8_t USART_ParityControl;
+	uint8_t USART_HWFlowControl;
+
+}USART_Config_t;
+
+
+/*
+ * Handle structure for USARTx peripheral
+ */
+typedef struct
+{
+	USART_RegDef_t *pUSARTx;
+	USART_Config_t   USART_Config;
+}USART_Handle_t;
+
+
+
+/*
+ *@USART_Mode
+ *Possible options for USART_Mode
+ */
+#define USART_MODE_ONLY_TX 0
+#define USART_MODE_ONLY_RX 1
+#define USART_MODE_TXRX  2
+
+/*
+ *@USART_Baud
+ *Possible options for USART_Baud
+ */
+#define USART_STD_BAUD_1200					1200
+#define USART_STD_BAUD_2400					400
+#define USART_STD_BAUD_9600					9600
+#define USART_STD_BAUD_19200 				19200
+#define USART_STD_BAUD_38400 				38400
+#define USART_STD_BAUD_57600 				57600
+#define USART_STD_BAUD_115200 				115200
+#define USART_STD_BAUD_230400 				230400
+#define USART_STD_BAUD_460800 				460800
+#define USART_STD_BAUD_921600 				921600
+#define USART_STD_BAUD_2M 					2000000
+#define SUART_STD_BAUD_3M 					3000000
+
+
+/*
+ *@USART_ParityControl
+ *Possible options for USART_ParityControl
+ */
+#define USART_PARITY_EN_ODD   2
+#define USART_PARITY_EN_EVEN  1
+#define USART_PARITY_DISABLE   0
+
+/*
+ *@USART_WordLength
+ *Possible options for USART_WordLength
+ */
+#define USART_WORDLEN_8BITS  0
+#define USART_WORDLEN_9BITS  1
+
+/*
+ *@USART_NoOfStopBits
+ *Possible options for USART_NoOfStopBits
+ */
+#define USART_STOPBITS_1     0
+#define USART_STOPBITS_0_5   1
+#define USART_STOPBITS_2     2
+#define USART_STOPBITS_1_5   3
+
+/*
+ *@USART_HWFlowControl
+ *Possible options for USART_HWFlowControl
+ */
+#define USART_HW_FLOW_CTRL_NONE    	0
+#define USART_HW_FLOW_CTRL_CTS    	1
+#define USART_HW_FLOW_CTRL_RTS    	2
+#define USART_HW_FLOW_CTRL_CTS_RTS	3
+
+
+
+/******************************************************************************************
+ *								APIs supported by this driver
+ *		 For more information about the APIs check the function definitions
+ ******************************************************************************************/
+/*
+ * Peripheral Clock setup
+ */
+void USART_PeriClockControl(USART_RegDef_t *pUSARTx, uint8_t EnorDi);
+
+/*
+ * Init and De-init
+ */
+void USART_Init(USART_Handle_t *pUSARTHandle);
+void USART_DeInit(USART_RegDef_t *pUSARTx);
+
+
+/*
+ * Data Send and Receive
+ */
+void USART_SendData(USART_RegDef_t *pUSARTx,uint8_t *pTxBuffer, uint32_t Len);
+void USART_ReceiveData(USART_RegDef_t *pUSARTx, uint8_t *pRxBuffer, uint32_t Len);
+
+void USART_Init(USART_Handle_t * pUSARTHandle);
+void USART_DeInit(USART_RegDef_t *pUSARTx);
+
+
+void USART_SendData(USART_Handle_t *pUSARTHandle, uint8_t *pTxBuffer, uint32_t Len)
+{
+
+	uint16_t *pdata;
+
+	for(uint32_t i = 0 ; i < Len; i++)
+	{
+
+
+while(!(  pUSARTHandle->pUSARTx->USART_SR &  1<<7));
+//Check the USART_WordLength item for 9BIT or 8BIT in a frame
+		if(pUSARTHandle->USART_Config.USART_WordLength== USART_WORDLEN_9BITS)
+		{
+			pdata = (uint16_t*) pTxBuffer;
+			pUSARTHandle->pUSARTx->USART_DR = (*pdata & (uint16_t)0x01FF);
+
+			//check for USART_ParityControl
+			if(pUSARTHandle->USART_Config.USART_ParityControl == USART_PARITY_DISABLE)
+			{
+				//No parity is used in this transfer. so, 9bits of user data will be sent
+				//Implement the code to increment pTxBuffer twice
+				pTxBuffer++;
+				pTxBuffer++;
+			}
+			else
+			{
+				//Parity bit is used in this transfer . so , 8bits of user data will be sent
+				//The 9th bit will be replaced by parity bit by the hardware
+				pTxBuffer++;
+			}
+		}
+		else
+		{
+			//This is 8bit data transfer
+			pUSARTHandle->pUSARTx->DR = (*pTxBuffer  & (uint8_t)0xFF);
+		    pTxBuffer++;
+		}
+	}
+
+	//wait till TC flag is set in the SR
+	while( !(pUSARTHandle->pUSARTx->USART_SR &  1<<5) );
+}
+
+
+
+void USART_ReceiveData(USART_Handle_t *pUSARTHandle, uint8_t *pRxBuffer, uint32_t Len)
+{
+   // "Len" number of bytes are transferred
+	for(uint32_t i = 0 ; i < Len; i++)
+	{
+		// wait until RXNE flag is set in the SR
+		while(! (pUSARTHandle->pUSARTx->USART_SR & 1<<5 ));
+
+		//CheckING USART_WordLength to decide whether we are going to receive 9bit of data in a frame or 8 bit
+		if(pUSARTHandle->USART_Config.USART_WordLength == USART_WORDLEN_9BITS)
+		{
+			//We are going to receive 9bit data in a frame
+			//check are we using USART_ParityControl control or not
+
+			if(pUSARTHandle->USART_Config.USART_ParityControl == USART_PARITY_DISABLE)
+			{
+				//No parity is used. so, all 9bits will be of user data
+				// mask the DR with 0x01FF
+
+				*((uint16_t*) pRxBuffer) = (pUSARTHandle->pUSARTx->DR  & (uint16_t)0x01FF);
+
+				// increment the pRxBuffer two times
+				pRxBuffer++;
+				pRxBuffer++;
+			}
+			else
+			{
+				//Parity is used so 8bits will be of user data and 1 bit is parity
+				 *pRxBuffer = (pUSARTHandle->pUSARTx->DR  & (uint8_t)0xFF);
+					pRxBuffer++;
+			}
+		}
+		else
+		{
+			//We are going to receive 8bit data in a frame
+
+			//check are we using USART_ParityControl control or not
+			if(pUSARTHandle->USART_Config.USART_ParityControl == USART_PARITY_DISABLE)
+			{
+				//No parity is used , so all 8bits will be of user data
+
+				//read 8 bits from DR
+				 *pRxBuffer = TODO;
+			}
+
+			else
+			{
+				//Parity is used, so , 7 bits will be of user data and 1 bit is parity
+
+				//read only 7 bits , hence mask the DR with 0X7F
+				 *pRxBuffer = (uint8_t)(pUSARTHandle->pUSARTx->DR  & (uint8_t)0x7F);
+
+			}
+
+			//increment the pRxBuffer
+			pRxBuffer++;
+		}
+	}
+
+}
+
+
+
+
+
+void USART_Init(USART_Handle_t *pUSARTHandle)
+{
+
+	//Temporary variable
+	uint32_t tempreg=0;
+
+/******************************** Configuration of CR1******************************************/
+
+	//Implement the code to enable the Clock for given USART peripheral
+	 TODO
+
+	//Enable USART Tx and Rx engines according to the USART_Mode configuration item
+	if ( pUSARTHandle->USART_Config.USART_Mode == USART_MODE_ONLY_RX)
+	{
+		//Implement the code to enable the Receiver bit field
+		tempreg|= (1 << 2);
+	}else if (pUSARTHandle->USART_Config.USART_Mode == USART_MODE_ONLY_TX)
+	{
+		//Implement the code to enable the Transmitter bit field
+		tempreg |= ( 1 << 3 );
+
+	}else if (pUSARTHandle->USART_Config.USART_Mode == USART_MODE_TXRX)
+	{
+		//Implement the code to enable the both Transmitter and Receiver bit fields
+
+		tempreg|= (1 << 2);
+		tempreg |= ( 1 << 3 );
+	}
+
+    //Implement the code to configure the Word length configuration item
+	tempreg |= pUSARTHandle->USART_Config.USART_WordLength << 12 ;
+
+
+    //Configuration of parity control bit fields
+	if ( pUSARTHandle->USART_Config.USART_ParityControl == USART_PARITY_EN_EVEN)
+	{
+		//Implement the code to enale the parity control
+		tempreg |= ( 1 << 10 );
+	    tempreg &= ~( 1 << 9);
+
+	}else if (pUSARTHandle->USART_Config.USART_ParityControl == USART_PARITY_EN_ODD )
+	{
+		// enable the parity control
+	    tempreg |= ( 1 << USART_CR1_PCE);
+
+	    //Implement the code to enable ODD parity
+	    tempreg |= ( 1 << 9);
+
+	}
+
+   // configuring the temp register
+	pUSARTHandle->pUSARTx->CR1 = temreg;
+
+/******************************** Configuration of CR2******************************************/
+
+	tempreg=0;
+
+	//configure the number of stop bits inserted during USART frame transmission
+	tempreg |= pUSARTHandle->USART_Config.USART_NoOfStopBits << 12 ;
+
+	//Program the CR2 register
+	pUSARTHandle->pUSARTx->USART_CR2 = tempreg;
+
+
+/******************************** Configuration of CR3******************************************/
+
+	tempreg=0;
+
+	//Configuration of USART hardware flow control
+	if ( pUSARTHandle->USART_Config.USART_HWFlowControl == USART_HW_FLOW_CTRL_CTS)
+	{
+		// code to enable CTS flow control
+		tempreg |= ( 1 << 9);
+
+
+	}else if (pUSARTHandle->USART_Config.USART_HWFlowControl == USART_HW_FLOW_CTRL_RTS)
+	{
+		//Implement the code to enable RTS flow control
+		tempreg |= (1<<8);
+
+	}else if (pUSARTHandle->USART_Config.USART_HWFlowControl == USART_HW_FLOW_CTRL_CTS_RTS)
+	{
+		//Implement the code to enable both CTS and RTS Flow control
+		tempreg|=(1<<9);
+		tempreg|=(1<<8);
+	}
+
+
+	pUSARTHandle->pUSARTx->USART_CR3 = tempreg;
+
+
+
+}
+
+
+#endif /* INC_USART_DRIVER_H_ */
